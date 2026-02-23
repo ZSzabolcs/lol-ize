f76591ef5a4e2b766a5bf2422a618e993abeb50c
ZSzabolcs <zelenaksz@kkszki.hu>
2026-02-23 08:27:30 +0100

    Elveszett ido

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Globalization;
+﻿using ConsoleApp1.Models;
+using System.Globalization;
 using System.Text.Json;
 using System.Threading.Tasks;
 
@@ -11,6 +12,33 @@ namespace ConsoleApp1
         {
             await LoadVersion();
             Console.WriteLine(version);
+            await LoadChampions();
+        }
+
+        static async Task LoadChampions()
+        {
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    client.Timeout = TimeSpan.FromSeconds(30);
+                    string url = $"https://ddragon.leagueoflegends.com/cdn/{version}/data/en_US/champion.json";
+                    var responseAPI = await client.GetStringAsync(url);
+                    var response = JsonSerializer.Deserialize<Champion[]>(responseAPI);
+                }
+            }
+            catch (HttpRequestException httpEx)
+            {
+                Console.WriteLine($"Kapcsolódási hiba: {httpEx.Message}");
+            }
+            catch (JsonException jsonEx)
+            {
+                Console.WriteLine($"Átalakítási hiba: {jsonEx.Message}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Hiba: {ex.Message}");
+            }
         }
 
         static async Task LoadVersion()
@@ -20,7 +48,7 @@ namespace ConsoleApp1
                 using (HttpClient client = new HttpClient())
                 {
                     client.Timeout = TimeSpan.FromSeconds(30);
-                    string url = "https://ddragon.leagueoflegends.com/api/versions.json";
+                    string url = $"https://ddragon.leagueoflegends.com/api/versions.json";
                     var responseAPI = await client.GetStringAsync(url);
                     string[] response = JsonSerializer.Deserialize<string[]>(responseAPI);
                     version = response[0];
